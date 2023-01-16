33da8d3e3a7114417b4b391457dda04932375731
Anupam Mazumdar <anupam.mazumdar@live.com>
2023-01-16 20:54:05 +0530

    Lidar capability added

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -37,6 +37,8 @@ def generate_launch_description():
                                    '-entity', 'mobile_bot'],
                         output='screen')
 
+    xbox_joystick = IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name), 'launch', 'joystick.launch.py')]))
 
 
     # Launch them all!
@@ -44,4 +46,5 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        xbox_joystick
     ])
